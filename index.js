89965ee8150483c9e1f527b43e95a8f25deea848
Your Name <you@example.com>
2020-09-15 12:19:36 -0500

    Well that didn't work

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function largestSubarraySum(array){
 
     let newArray = []
+    let comparativeArray = []
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
     array.forEach(element => {
+        comparativeArray.push(element)
         if (element > 0){
         newArray.push(element)}
     });
-    // This is a comment just for a commit
+    // let finalArray = []
+    // if (newArray > comparativeArray){
+    //     finalArray = newArray
+    // }else if (comparativeArray > newArray) {
+    //     finalArray = comparativeArray
+    // }
+    // return finalArray.reduce(reducer)
     return newArray.reduce(reducer)
    }
